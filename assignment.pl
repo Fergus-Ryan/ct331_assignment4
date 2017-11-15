0ae8c9ae1e83d9beca8612b7a67def81d24262ae
Fergus-Ryan <fergusryan@live.ie>
2017-11-15 17:29:26 +0000

    Assignment4 code

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,19 @@
 %Definitions for isElementInList(El, List)
-
+isElementInList(El, List) :-
+    member(El, List).
+    
 %Definitions for reverseList(List, ReversedList)
+reverseList([], []).
+reverseList(List, ReversedList) :-
+    [H|T] = List,
+    reverseList(T, Z),
+    mergeLists(Z, [H], ReversedList).
 
 %insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd(El, List, NewList):-
+    mergeLists(List, El, NewList).
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists(List1, List2, Merged) :-
+   	L = [List1, List2],
+    flatten(L, Merged).
